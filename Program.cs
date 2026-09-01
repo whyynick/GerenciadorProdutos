36ba77baba1058418b65038b0206b963da1566b3
Nicolas <franchinigamer@gmail.com>
2026-09-01 11:09:46 -0400

    Atualiza o projeto e insere .gitignore

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ void ListarProdutos()
     {
         Console.WriteLine($"ID: {p.Value.Id}");
         Console.WriteLine($"Nome: {p.Value.Nome}");
-        Console.WriteLine($"Preço: {p.Value.Preco}");
+        Console.WriteLine($"Preço: R$ {p.Value.Preco:F2}");
     }
 }
 
@@ -25,4 +25,68 @@ produtos[produto04.Id] = produto04;
 produtos[produto05.Id] = produto05;
 
 
-ListarProdutos();
+bool menu = true;
+
+while(menu == true){
+
+    Console.WriteLine("========= - MENU - =========");
+    Console.WriteLine("1 - Listar produtos");
+    Console.WriteLine("2 - Buscar produto por ID");
+    Console.WriteLine("3 - Excluir produto");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("============================");
+
+    string opt = Console.ReadLine();
+
+    switch(opt){
+
+        case "1":
+            ListarProdutos();
+            break;
+
+        case "2":
+            Console.Write("Digite o número de ID do item desejado:");
+            int IdBuscado = int.Parse(Console.ReadLine());
+
+                if (produtos.ContainsKey(IdBuscado))
+                {
+                    var encontrado = produtos[IdBuscado];
+                    Console.WriteLine($"ID: {encontrado.Id}\nNome: {encontrado.Nome}\nPreço: R$ {encontrado.Preco:F2}");
+                }
+
+                else
+                {
+                    Console.WriteLine("Não existe nenhum produto com esse ID cadastado!");
+                }
+            break;
+        
+        case "3":
+            Console.WriteLine("Digite o ID do produto que deseja remover:");
+            int IdRemover = int.Parse(Console.ReadLine());
+
+                if (produtos.ContainsKey(IdRemover)){
+                    string? nomeRemovido = produtos[IdRemover].Nome;
+                    produtos.Remove(IdRemover);
+                    Console.WriteLine($"Produto {nomeRemovido} removido com sucesso!");
+                }
+
+                else{
+                    Console.WriteLine("Não existe nenhum produto com esse ID cadastrado!");
+                }
+            break;
+
+        case "0":
+            menu = false;
+            Console.WriteLine("Menu encerrado.");
+            break;
+        
+        default:
+            Console.WriteLine("Opção inválida, por favor escolha uma opção de 0 a 3 no menu!");
+            break;
+        
+    }
+};
+
+
+
+
